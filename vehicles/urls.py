a2b260f642ff6d669225c2c01ab84e7cb8e863cd
Vinuvi Nuhansa <vinuvidandeniya@gmail.com>
2024-06-10 12:00:39 +0530

    added a new vehicle_details.html

--- vehicles/urls.py
+++ vehicles/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('submit/', views.submit_listing, name='submit_listing'),
+    path('vehicle_detail/<int:vehicle_id>/', views.vehicle_detail, name='vehicle_detail'),
 ]
--- vehicles/views.py
+++ vehicles/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Vehicle
 
 def index(request):
     return render(request, 'vehicles/vehicle_listing.html')
@@ -8,3 +9,10 @@ def submit_listing(request):
     # Process the form submission here
     # For example, save the listing data to the database
     return HttpResponse("Listing submitted successfully!")
+
+def vehicle_detail(request, vehicle_id):
+    vehicle = get_object_or_404(Vehicle, pk=vehicle_id)
+    context = {
+        'vehicle': vehicle
+    }
+    return render(request, 'vehicles/vehicle_details.html', context)
