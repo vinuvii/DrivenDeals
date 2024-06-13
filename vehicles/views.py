c1f588e70a8afcaaea3ab974159ce6bb2bdc6301
Vinuvi Nuhansa <vinuvidandeniya@gmail.com>
2024-06-14 01:19:17 +0530

    links updated in header

--- vehicles/views.py
+++ vehicles/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages  # Import messages
 from django.contrib.auth.decorators import login_required
 from .models import Vehicle
 from .forms import VehicleForm
+from django.urls import reverse
 
 def index(request):
     return render(request, 'vehicles/vehicle_listing.html')
@@ -27,9 +28,39 @@ def vehicle_success(request):
 
 def vehicle_detail(request, vehicle_id):
     vehicle = get_object_or_404(Vehicle, pk=vehicle_id)
+
     context = {
         'is_authenticated': request.user.is_authenticated,
-        #'is_authenticated': False,
         'vehicle': vehicle,
     }
+
+    if request.method == 'POST' and not request.user.is_authenticated:
+        # If it's a POST request and user is not authenticated, redirect to login
+        login_url = reverse('login')  # Assuming 'login' is your login URL name
+        return redirect(f"{login_url}?next={request.path}")
+
     return render(request, 'vehicles/vehicle_details.html', context)
+
+def home(request):
+    return render(request, 'vehicles/home.html')
+
+
+@login_required
+def watchlist_view(request):
+    # Logic to fetch data for watchlist if needed
+    return render(request, 'user/watchlist.html')
+
+@login_required
+def my_listings_view(request):
+    # Logic to fetch data for user's listings if needed
+    return render(request, 'user/my_listings.html')
+
+@login_required
+def my_bids_view(request):
+    # Logic to fetch data for user's bids if needed
+    return render(request, 'user/my_bids.html')
+
+@login_required
+def edit_profile_view(request):
+    # Logic to fetch data for user's bids if needed
+    return render(request, 'user/edit_profile.html')
